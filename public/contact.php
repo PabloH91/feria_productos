07e907015702dcb3e0b1d09d702d24954b361bc9
Pablo H <hernandez.pah91@gmail.com>
2015-02-10 14:54:54 -0600

    Add about and contact pages with lorem ipsum.

---
+++ public/contact.php
@@ -0,0 +1,35 @@
+<?php require_once("../app/config.php"); ?>
+<?php require_once("../app/functions.php"); ?>
+
+<?php include(TEMPLATES_PATH . "/header.php"); ?>
+<div class="container">
+	<div class="row">
+		<article class="col-sm-8 col-sm-offset-1 col-sm-push-3">
+			<h3>Article</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa natus facilis id omnis error neque alias quam debitis labore laudantium dicta est molestias quibusdam laboriosam, accusantium architecto, nostrum animi? Et!</p>
+			<div class="row">
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<h3>Section #1</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, porro voluptatibus sed, at quis accusantium voluptatem delectus exercitationem nostrum magni pariatur ipsum, quaerat asperiores adipisci iure qui eligendi! Dolor, architecto.</p>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<h3>Section #2</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, porro voluptatibus sed, at quis accusantium voluptatem delectus exercitationem nostrum magni pariatur ipsum, quaerat asperiores adipisci iure qui eligendi! Dolor, architecto.</p>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<h3>Section #3</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, porro voluptatibus sed, at quis accusantium voluptatem delectus exercitationem nostrum magni pariatur ipsum, quaerat asperiores adipisci iure qui eligendi! Dolor, architecto.</p>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<h3>Section #4</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, porro voluptatibus sed, at quis accusantium voluptatem delectus exercitationem nostrum magni pariatur ipsum, quaerat asperiores adipisci iure qui eligendi! Dolor, architecto.</p>
+				</div>
+			</div>
+		</article>
+		<aside class="col-sm-3 col-sm-pull-9">
+			<h3>Aside</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, nemo, nesciunt? Labore non quia, veniam nobis nesciunt quasi enim quis ut adipisci, consequuntur dolores, earum, sit asperiores nemo voluptatibus! Est.</p>
+		</aside>
+	</div>
+</div>
+<?php include(TEMPLATES_PATH . "/footer.php"); ?>
